4b054b6e5db9d4297923497ebd15012d48b28327
space-9097 <88567941+space-9097@users.noreply.github.com>
2021-09-24 19:20:49 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var s1;
+var s2;
+
+function setup() {
+  createCanvas(400,400);
+  s1=new Student("J",10,5)
+  s1.display()
+  s2=new Student("K",11,6)
+  s2.display()
+}
+
+function draw() 
+{
+  background(30);
+
+}
